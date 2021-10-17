aa2c4b3c28df3c4890c57e370a9c4486099e5053
Nico Wagner <info@ni-wa.de>
2021-10-17 23:28:19 +0200

    Updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
+import PrinterManager from "./lib/PrinterManager";
+import {IPrinterConfig} from "./interfaces/IPrinterConfig";
+import Mqtt from "./dataTechnologies/mqtt";
+const config = require("../config.js");
 console.log("[%s] Starting Process", process.uptime());
+
+new Mqtt(config.mqtt.host,config.mqtt.port, config.mqtt.clientID, config.mqtt.username, config.mqtt.password, config.mqtt.topic);
+
+const printerManager = new PrinterManager();
+
+config.printers.forEach((e: IPrinterConfig) => {
+    const u = printerManager.addPrinter(new e.type(e.name, e.url, e.sendToDataTechnologies || [], e.dataSaveMode), e.interval);
+});
+
+process.on("SIGINT", () => {
+    console.log("\n[%s] Stopping Programm...\n\n.", process.uptime());
+    process.exit(0);
+});
